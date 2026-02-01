ec5d45c7c1b90911ee5b9d3775590c071026a586
sachigoyal <sachigoyal27@gmail.com>
2026-02-01 22:14:33 +0600

    feat: projects page

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,34 @@
+import { projects } from "#site/content";
+import { PageHeader, ProjectsList } from "@/components/sections";
+import type { Metadata } from "next";
+import { siteConfig } from "@/config/site.config";
+import Link from "next/link";
+import { ArrowLeft } from "lucide-react";
+
+export const metadata: Metadata = {
+  title: `Projects | ${siteConfig.name}`,
+  description: "Projects I've built.",
+  openGraph: {
+    title: `Projects | ${siteConfig.name}`,
+    description: "Projects I've built.",
+    url: `${siteConfig.origin}/projects`,
+  },
+};
+
+export default function ProjectsPage() {
+  return (
+    <main className="flex flex-col items-center pt-12 md:pt-24 lg:pt-32 pb-10 px-3 md:px-0">
+      <div className="relative max-w-2xl w-full flex flex-col">
+        <PageHeader />
+
+        <div className="absolute h-full w-10 -left-12 top-14">
+          <Link href="/" className="sticky top-4">
+            <ArrowLeft className="size-4" />
+          </Link>
+        </div>
+
+        <ProjectsList projects={projects} />
+      </div>
+    </main>
+  );
+}
