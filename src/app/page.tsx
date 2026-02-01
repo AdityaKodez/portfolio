2a7377d63103b97c2da5e69150f08ce1381dbdc6
sachigoyal <sachigoyal27@gmail.com>
2026-02-01 22:15:00 +0600

    feat: basic page structure

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import { PageHeader } from "@/components/sections/page-header";
+import { Intro } from "@/components/sections/intro";
+import { About } from "@/components/sections/about";
+import { Projects } from "@/components/sections/projects";
+import { RecentBlogs } from "@/components/sections/recent-blogs";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center pt-12 md:pt-24 lg:pt-32 pb-10 px-3 md:px-0">
+      <div className="max-w-2xl w-full flex flex-col">
+        <PageHeader className="mb-4" />
+        <Intro />
+        <About />
+        <Projects />
+        <RecentBlogs />
+      </div>
+    </main>
+  );
+}
